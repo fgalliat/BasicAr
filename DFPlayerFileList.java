8fd5f590a81d7cc39aaab479e82d62cb700c141e
fgalliat <fgalliat@gmail.com>
2018-03-12 20:14:20 +0100

    ADDED MP3 JUKEbox collection file generator

--- DFPlayerFileList.java
+++ DFPlayerFileList.java
@@ -3,6 +3,8 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class DFPlayerFileList {
 
@@ -20,18 +22,55 @@ public class DFPlayerFileList {
                 result.add( content[i].getName() );
             }
         }
+
+        FileOutputStream outFileOs = new FileOutputStream( outFile );
+        PrintStream outF = new PrintStream(outFileOs);
+
         System.out.println(result.size());
+        outF.println(result.size());
         System.out.println("# idx / name");
+        outF.println("# idx / name");
         Iterator<String> itFile = result.iterator();
         for(int i=0; itFile.hasNext(); i++) {
             String name = itFile.next();
             String num = (i < 9 ? "0" : "")+ (i+1);
             System.out.println(num+";"+name);
+            outF.println(num+";"+name);
         }
+
+        outFileOs.flush();
+        outFileOs.close();
     }
+
+    public static void reindexDir(String dir) throws Exception {
+        File f = new File(dir);
+        File[] content = f.listFiles();
+        Set<String> result = new TreeSet<String>( new Comparator<String>() {
+            public int compare(String e0, String e1) {
+                return e0.compareTo(e1);
+            }
+        } );
+        for(int i=0; i < content.length; i++) {
+            if ( content[i].getName().toLowerCase().endsWith(".mp3") ) {
+                result.add( content[i].getName() );
+            }
+        }
+
+        Iterator<String> itFile = result.iterator();
+        for(int i=0; itFile.hasNext(); i++) {
+            String name = itFile.next();
+            String num = (i < 9 ? "0" : "")+ (i+1);
+            if ( name.indexOf("_") != 2 ) { continue; }
+            String newName = num + name.substring(2);
+            System.out.println(num+"| "+name+"   =>   "+newName);
+
+            if ( true ) { new File(f, name).renameTo(new File(f, newName)); }
+        }
+    } // end of reindexDir()
 
 
     public static void main(String[] args) throws Exception {
+        //reindexDir("./MP3");
         treatDir("./MP3", "./data/JUKE.BAD");
     }
 
